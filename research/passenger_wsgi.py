11fb2c207a2d3bfe9fea2f4ec2451c38a6bb4dc3
ebtrader <jsiddique@gmail.com>
2023-06-18 10:36:24 -0400

    Commit

--- research/passenger_wsgi.py
+++ research/passenger_wsgi.py
@@ -5,26 +5,60 @@ if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
 
-from flask import Flask, render_template, request
-import yfinance as yf
-import pandas as pd
-import plotly.graph_objects as go
-import numpy as np
-import math
+# Start with a basic flask app webpage.
+from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, url_for, copy_current_request_context
+from random import random
+from time import sleep
+from threading import Thread, Event
+
+__author__ = 'slynn'
 
 application = Flask(__name__)
+application.config['SECRET_KEY'] = 'secret!'
+application.config['DEBUG'] = True
 
-@application.route('/simple')
-def output_simple():
-    out_text = 'hello world now'
-    return render_template('output.html', arithmetic=out_text)
+#turn the flask app into a socketio app
+socketio = SocketIO(application, async_mode=None, logger=True, engineio_logger=True)
 
-@application.route('/chain')
-def opt_chain():
-    return "Hello World"
+#random number Generator Thread
+thread = Thread()
+thread_stop_event = Event()
 
-@application.route('/numbers')
-def hello():
-    ticker = 'NQ=F'
-    data = yf.download(tickers=ticker, period='6mo')
-    return data.to_html(header='true', table_id='table')
+def randomNumberGenerator():
+    """
+    Generate a random number every 2 seconds and emit to a socketio instance (broadcast)
+    Ideally to be run in a separate thread?
+    """
+    #infinite loop of magical random numbers
+    print("Making random numbers")
+    while not thread_stop_event.isSet():
+        number = round(random()*10, 3)
+        print(number)
+        socketio.emit('newnumber', {'number': number}, namespace='/test')
+        socketio.sleep(2)
+
+
+@application.route('/')
+def index():
+    #only by sending this page first will the client be connected to the socketio instance
+    return render_template('index.html')
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    # need visibility of the global thread object
+    global thread
+    print('Client connected')
+
+    #Start the random number generator thread only if the thread has not been started before.
+    if not thread.is_alive():
+        print("Starting Thread")
+        thread = socketio.start_background_task(randomNumberGenerator)
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    socketio.run(application, port=8080)
